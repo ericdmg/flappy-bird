08e390617692ea066257d82ec6c0f64299190e4f
eric <eric.goncalves@ccc.ufcg.edu.br>
2020-07-25 02:28:24 -0300

    feat/02 add create barrier

--- js/flappy.js
+++ js/flappy.js
@@ -4,3 +4,17 @@ function createNewElement(tagName, className){
     return elem
 }
 
+function Barrier(reverse = false){
+    this.element = createNewElement('div', 'barrier')
+    const mouthpiece = createNewElement('div', 'mouthpiece')
+    const pipe = createNewElement('div', 'pipe')
+
+    this.element.appendChild(reverse ? pipe : mouthpiece)
+    this.element.appendChild(reverse ? mouthpiece : pipe)
+
+    this.setHeight = height => pipe.style.height = `${height}px`
+}
+
+// const b = new Barrier(true)
+// b.setHeight(200)
+// document.querySelector('[wm-flappy]').appendChild(b.element)
